0192565b75af556194a4464e5e9ab8ef10e48609
Vashishta Mithra Reddy Vundyala <vashishtamithrareddy@gmail.com>
2025-10-07 02:08:50 +0530

    webauthn

--- app/dashboard/DashboardClient.tsx
+++ app/dashboard/DashboardClient.tsx
@@ -1,57 +1,14 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { useEncryption } from "../providers/EncryptionProvider";
 import UnlockVaultForm from "@/components/UnlockVaultForm";
 import PasskeySetup from "@/components/webauthn/PassKeySetup";
-import { authClient } from "@/lib/auth-client"; 
-import Spinner from "@/components/Spinner";
 
 export default function DashboardClient() {
   const { isLocked } = useEncryption();
 
-  const [showPasskeySetup, setShowPasskeySetup] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const checkPasskeyAvailability = async () => {
-      try {
-        const supportsWebAuthn =
-          typeof window.PublicKeyCredential !== "undefined" &&
-          PublicKeyCredential.isConditionalMediationAvailable &&
-          (await PublicKeyCredential.isConditionalMediationAvailable());
-
-        if (!supportsWebAuthn) {
-          return setShowPasskeySetup(false);
-        }
-
-        const { data: passkeys, error } = await authClient.passkey.listUserPasskeys();
-
-        if (error) {
-          console.warn("Error listing passkeys:", error);
-          return setShowPasskeySetup(false); 
-        }
-
-        const hasPasskey = Array.isArray(passkeys) && passkeys.length > 0;
-        setShowPasskeySetup(!hasPasskey);
-        
-      } catch (err) {
-        console.error("Error checking passkey support or setup:", err);
-        setShowPasskeySetup(false);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    void checkPasskeyAvailability();
-  }, []);
-
   if (isLocked) {
     return <UnlockVaultForm />;
-  }
-
-  if (loading) {
-    return <Spinner />
   }
 
   return (
@@ -65,15 +22,14 @@ export default function DashboardClient() {
         </p>
       </section>
 
-      {showPasskeySetup && (
+      {/* {!("PublicKeyCredential" in window) && ( */}
         <section className="p-6 py-12 rounded-xl border-2 border-dashed border-foreground/20">
           <h2 className="text-3xl font-semibold mb-8 text-center">
             Passkey Management
           </h2>
-          {/* <PasskeySetup onSuccess={() => setShowPasskeySetup(false)} /> */}
-          <PasskeySetup/>
+          <PasskeySetup />
         </section>
-      )}
+      {/* )} */}
     </div>
   );
 }
